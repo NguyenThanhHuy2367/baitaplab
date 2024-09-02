5d1230747b6a4df5d43fe07e798b8afe9ec55330
NguyenThanhHuy2367 <nguyenthanhhuy12a12023@gmail.com>
2024-09-02 12:54:41 +0700

    Add files via upload

---
+++ LAB01.B26B.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 12:52:55 2024
+
+@author: NGUYEN THANH HUY
+"""
+
+N = input("Nhập số nguyên N: ")
+tangdan = ''.join(sorted(N))
+print("Số có các con số theo thứ tự tăng dần: ", tangdan)
